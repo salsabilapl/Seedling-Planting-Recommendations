e4d6bc80b0b652926f93da290d75077208ef692b
salsabilapl <lesmarnasalsabila@gmail.com>
2023-07-02 12:29:02 +0700

    modif

--- scripts/run-feature-and-prediction-pipelines.sh
+++ scripts/run-feature-and-prediction-pipelines.sh
@@ -3,5 +3,5 @@
 set -e
 
 
-jupyter nbconvert --to notebook --execute Klasifikasi-Feature-Pipeline.ipynb
-jupyter nbconvert --to notebook --execute Klasifikasi-Batch-Inference-Pipeline.ipynb
+jupyter nbconvert --to notebook --executePreprocessor.kernel_name=python3 Klasifikasi-Feature-Pipeline.ipynb
+jupyter nbconvert --to notebook --executePreprocessor.kernel_name=python3 Klasifikasi-Batch-Inference-Pipeline.ipynb
